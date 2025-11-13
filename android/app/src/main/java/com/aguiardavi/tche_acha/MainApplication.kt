03b34ca4d4137d054c73e11b08a07af902c41102
Davi Oliveira <daviim0510@gmail.com>
2025-11-12 21:44:01 -0300

    fix main app kt

--- android/app/src/main/java/com/aguiardavi/tche_acha/MainApplication.kt
+++ android/app/src/main/java/com/aguiardavi/tche_acha/MainApplication.kt
@@ -3,8 +3,6 @@ package com.aguiardavi.tche_acha
 import android.app.Application
 import android.content.res.Configuration
 
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.loadReactNative
-import com.facebook.react.defaults.DefaultReactNativeApplicationEntryPoint
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
